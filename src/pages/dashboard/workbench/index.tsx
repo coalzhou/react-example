e11db32acb8160d8e48337aa3ca8b51d6180e9f2
coalzhou <coal_2010@163.com>
2025-02-17 20:41:04 +0800

    2 hours

--- src/pages/dashboard/workbench/index.tsx
+++ src/pages/dashboard/workbench/index.tsx
@@ -9,9 +9,26 @@ import TopAuthor from "./top-authors";
 import TopInstalled from "./top-installed";
 import TopRelated from "./top-related";
 import TotalCard from "./total-card";*/
-import Paper from "@mui/material/Paper";
-import Divider from "@mui/material/Divider";
+import {Paper, Divider,FormControl,InputLabel,Input,FormHelperText,Button,Select,MenuItem,TableContainer,
+	Table,TableHead,TableRow,TableCell,TableBody
+} from "@mui/material";
 
+const handleClick = () => {
+	console.log('hello')
+}
+const selectChange = (e) =>{
+	console.log(e.target.value)
+}
+const rows = [
+	{name: 'row1', age: 18, sex: 'male'},
+	{name: 'row2', age: 19, sex: 'male'},
+	{name: 'row3', age: 18, sex: 'female'},
+	{name: 'row4', age: 21, sex: 'female'},
+]
+const clickRow = (record) =>{
+	debugger
+	console.log('h world: ' + JSON.stringify(record))
+}
 function Workbench() {
 	return (
 		<div className="p-2">
@@ -22,6 +39,67 @@ function Workbench() {
 				このプロジェクトでは、メインの UI ライブラリとして ANTD を使用しています。こちらをご覧ください:
 				https://ant.design/components/overview-cn/
 			</Paper>
+			<div className="mt-2">
+			<FormControl>
+				<InputLabel htmlFor="yourname">username</InputLabel>
+				<Input id="yourname" aria-describedby="username-help" />
+				<FormHelperText id="username-help">please input your name</FormHelperText>
+			</FormControl>
+				<FormControl style={{ width: "80px", marginLeft: "20px" }} >
+					<InputLabel htmlFor="select-age-id">Age</InputLabel>
+					<Select
+						labelId="select-age-id"
+						id="demo-simple-select"
+						value=""
+						onChange={selectChange}
+						>
+						<MenuItem value={10}>10</MenuItem>
+						<MenuItem value={20}>20</MenuItem>
+						<MenuItem value={30}>30</MenuItem>
+					</Select>
+				</FormControl>
+
+				<div>
+				<Button variant="contained" color="primary" onClick={handleClick}>
+					please click
+				</Button>
+				</div>
+
+			</div>
+			<Divider component="li" />
+			<TableContainer component={Paper} style={{marginTop: "40px"}}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell align="center">username</TableCell>
+							<TableCell align="center">age</TableCell>
+							<TableCell align="center">sex</TableCell>
+							<TableCell align="center">operation</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{rows.map((row) => (
+							<TableRow
+								key={row.name}
+								sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+							>
+								<TableCell component="th" scope="row" align="center">
+									{row.name}
+								</TableCell>
+								<TableCell align="center">{row.age}</TableCell>
+								<TableCell align="center">{row.sex}</TableCell>
+								<TableCell align="center" scope="row" component="th">
+									<Button variant="contained" color="primary" onClick={()=>clickRow(row)}>
+										click Me
+									</Button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+
+			</TableContainer>
+
 		</div>
 	);
 	/*return (
